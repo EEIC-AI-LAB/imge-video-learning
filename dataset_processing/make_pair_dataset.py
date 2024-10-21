83f994859c41a477354737963687782877546e18
Omusubi0123 <Vermouth77rseidl@gmail.com>
2024-10-21 23:42:38 +0900

    feat: pair frameとしてデータセット作成

---
+++ dataset_processing/make_pair_dataset.py
@@ -0,0 +1,25 @@
+# 全動画について、3秒間のフレームを取得し、その中で差分が大きいフレーム20個を抽出する
+from pathlib import Path
+import os
+import shutil
+
+
+source_frames_dir = Path.cwd() / "data/dataset_baseball_pitch"
+save_frames_dir = Path.cwd() / "data/dataset"
+save_frames_dir.mkdir(exist_ok=True)
+
+for dataset_type in ["train", "evaluation", "test"]:
+    output_dir = save_frames_dir / dataset_type
+    (output_dir / "input").mkdir(parents=True, exist_ok=True)
+    (output_dir / "target").mkdir(parents=True, exist_ok=True)
+    for videoname_dir in (source_frames_dir / dataset_type).iterdir():
+        videoname_list = sorted(list(videoname_dir.rglob("*.png")))
+        shutil.copy(videoname_list[0], output_dir / "input" / f"{videoname_list[0].stem}_input.png")
+        for i, file_path in enumerate(videoname_list[1:-1]):
+            input_frame_path = output_dir / "input" / f"{file_path.stem}_{i:03d}_input.png"
+            target_frame_path = output_dir / "target" / f"{file_path.stem}_{i:03d}_target.png"
+            shutil.copy(file_path, input_frame_path)
+            shutil.copy(file_path, target_frame_path)
+        shutil.copy(videoname_list[-1], output_dir / "target" / f"{videoname_list[-1].stem}_target.png")
+
+        print(f"Saved {videoname_dir} to {output_dir}")
---
+++ dataset_processing/split_videos.py
@@ -0,0 +1,42 @@
+# 50種のアクションから各種3つの動画を抽出する
+from pathlib import Path
+import random
+import re
+import shutil
+
+source_dir = Path("/mnt/c/Users/Pocha/Downloads/UCF50")
+new_folder = Path.cwd() / "data/videos_baseball_pitch"
+action_type = "BaseballPitch"
+
+train_folder = new_folder / "train"
+evaluation_folder = new_folder / "evaluation"
+test_folder = new_folder / "test"
+train_folder.mkdir(parents=True, exist_ok=True)
+evaluation_folder.mkdir(parents=True, exist_ok=True)
+test_folder.mkdir(parents=True, exist_ok=True)
+
+pattern = f"v_{action_type}+_g[0-9]+_c[0-9]+\.avi"
+action_type_dir = source_dir / action_type
+all_files = [f_path for f_path in action_type_dir.rglob("*.avi") if re.match(pattern, f_path.name)]
+random.shuffle(all_files)
+
+train_split = int(0.8 * len(all_files))
+eval_split = int(0.1 * len(all_files))
+
+train_files = all_files[:train_split]
+eval_files = all_files[train_split:train_split + eval_split]
+test_files = all_files[train_split + eval_split:]
+
+for f_path in train_files:
+    shutil.copy2(f_path, train_folder)
+    print(f"Copied {f_path.name} to {train_folder}")
+
+for f_path in eval_files:
+    shutil.copy2(f_path, evaluation_folder)
+    print(f"Copied {f_path.name} to {evaluation_folder}")
+
+for f_path in test_files:
+    shutil.copy2(f_path, test_folder)
+    print(f"Copied {f_path.name} to {test_folder}")
+
+print(f"All files copied to {new_folder}")
