c9064cf1ab7b8703ebb771542c7d44921647294c
Omusubi0123 <Vermouth77rseidl@gmail.com>
2024-10-21 11:58:44 +0900

    feat: 動画を抽出・全動画から20フレーム抽出

--- dataset_processing/video_to_frame.py
+++ dataset_processing/video_to_frame.py
@@ -1,48 +1,55 @@
+# 全動画について、3秒間のフレームを取得し、その中で差分が大きいフレーム20個を抽出する
+from pathlib import Path
 import os
 import numpy as np
 import cv2
 
-input_video_path = "/mnt/c/Users/Pocha/Downloads/UCF50/BaseballPitch/v_BaseballPitch_g01_c01.avi"
-output_dir = "data/frames"
-os.makedirs(output_dir, exist_ok=True)
 
-cap = cv2.VideoCapture(input_video_path)
-fps = cap.get(cv2.CAP_PROP_FPS)
+source_video_dir = Path.cwd() / "data/videos"
+save_frames_dir = Path.cwd() / "data/frames"
+save_frames_dir.mkdir(exist_ok=True)
 
-capture_seconds = 3
-num_frames = int(fps * capture_seconds)
+for file_path in source_video_dir.rglob("*.avi"):
+    output_dir = save_frames_dir / file_path.stem
+    output_dir.mkdir(exist_ok=True)
 
-frame_count = 0
-previout_frame = None
+    cap = cv2.VideoCapture(str(file_path))
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
-frame_list = []
-diff_list = []
-while cap.isOpened() and frame_count < num_frames:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    frame_list.append(frame)
-    
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    if previout_frame is not None:
-        frame_diff = cv2.absdiff(previout_frame, gray_frame)
-        diff_sum = np.sum(frame_diff)
-        diff_list.append(diff_sum)
+    capture_seconds = 3
+    num_frames = int(fps * capture_seconds)
+    num_extract_frame = 20
+
+    frame_count = 0
+    previout_frame = None
+
+    frame_list = []
+    diff_list = []
+
+    # 前のフレームとの差分を計算し、合計値の上位{num_extract_frame}フレームを保存
+    while cap.isOpened() and frame_count < num_frames:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_list.append(frame)
         
-        print(f"Frame {frame_count}: {diff_sum}")
-        previout_frame = gray_frame
-    else:
-        diff_list.append(-1)
-        previout_frame = gray_frame
-    frame_count += 1
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if previout_frame is not None:
+            frame_diff = cv2.absdiff(previout_frame, gray_frame)
+            diff_sum = np.sum(frame_diff)
+            diff_list.append(diff_sum)
+            previout_frame = gray_frame
+        else:
+            diff_list.append(-1)
+            previout_frame = gray_frame
+        frame_count += 1
 
-num_extract_frame = 20
-large_diff_idx = np.array(diff_list).argsort()[-num_extract_frame:][::-1]
+    # 差分の大きいフレームを保存
+    large_diff_idx = np.array(diff_list).argsort()[-num_extract_frame:][::-1]
+    for idx in large_diff_idx:
+        frame = frame_list[idx]
+        frame_path = os.path.join(output_dir, f"frame_{idx:04d}.png")
+        cv2.imwrite(frame_path, frame)
 
-for idx in large_diff_idx:
-    frame = frame_list[idx]
-    frame_path = os.path.join(output_dir, f"frame_{idx:04d}.png")
-    cv2.imwrite(frame_path, frame)
-
-cap.release()
-print(f"Saved {frame_count} frames to {output_dir}")
+    cap.release()
+    print(f"Saved {frame_count} frames to {output_dir}")
