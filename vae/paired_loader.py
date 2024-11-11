f4e6553907064afceddaaf84f37acbef3d038736
Omusubi0123 <Vermouth77rseidl@gmail.com>
2024-11-11 17:49:08 +0900

    feat: vae学習コード添付

---
+++ vae/paired_loader.py
@@ -0,0 +1,30 @@
+import os
+from torchvision import datasets, transforms
+from PIL import Image
+
+
+class PairedImageFolder(datasets.ImageFolder):
+    def __init__(self, root, transform=None):
+        self.input_dir = os.path.join(root, 'inputs')
+        self.target_dir = os.path.join(root, 'targets')
+        self.input_images = sorted(os.listdir(self.input_dir))
+        self.target_images = sorted(os.listdir(self.target_dir))
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.input_images)
+    
+    def __getitem__(self, index):
+        input_path = os.path.join(self.input_dir, self.input_images[index])
+        target_path = os.path.join(self.target_dir, self.target_images[index])
+        
+        input_image = Image.open(input_path).convert('RGB')
+        target_image = Image.open(target_path).convert('RGB')
+        
+        if self.transform:
+            input_image = self.transform(input_image)
+            target_image = self.transform(target_image)
+        
+        return input_image, target_image
+
+ 
